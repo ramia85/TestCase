6c76269c5c3548135fb9a7fa22b48e9f9d6597b1
Ramia <ramiaarnouk@yahoo.com>
2022-01-24 14:43:07 -0500

    New test case 002 second commit

--- src/NewTestCase/TC002.java
+++ src/NewTestCase/TC002.java
@@ -2,6 +2,8 @@ package NewTestCase;
 
 public class TC002 {
     public static void main(String[] args) {
-        System.out.println("This is first comit in TC 002");
+        System.out.println("This is first commit in TC 002");
+        System.out.println("This is second commit in TC 002");
+
     }
 }
